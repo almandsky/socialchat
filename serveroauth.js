040fa6a644c9d1a6ef5f0bc0f7f59e413c4e42ea
Sky Chen <tianxiong_chen@hotmail.com>
2012-12-12 11:00:09 +0800

    disable gzip

--- serveroauth.js
+++ serveroauth.js
@@ -512,7 +512,7 @@ var app = express.createServer(
 
 app.configure(function() {
   app.use(express.logger());
-  app.use(express.compress());
+//  app.use(express.compress());
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.static(__dirname + '/public'));
